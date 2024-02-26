508a6f5e95ea18fdf17600ddb42e1f303ff082d0
kimjh1234 <ran824200@gmail.com>
2024-02-26 15:59:20 +0900

    1단계 ã완료

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import java.util.Scanner;
+
+class App {
+    void run() {
+        System.out.println("프로그램 실행");
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령 ) ");
+        Scanner s = new Scanner(System.in);
+        String order = s.nextLine();
+    }
+}
