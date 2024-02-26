6239c1b2377abf7f41e00661d8f38d64978df060
kimjh1234 <ran824200@gmail.com>
2024-02-26 16:09:28 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,8 +6,19 @@ class App {
     void run() {
         System.out.println("프로그램 실행");
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령 ) ");
-        Scanner s = new Scanner(System.in);
-        String order = s.nextLine();
+        for ( ; ; ){
+            System.out.print("명령 ) ");
+            Scanner s = new Scanner(System.in);
+            String 명령 = s.nextLine();
+            if (명령.equals("종료")) {
+                break;
+            } else if (명령.equals("등록")){
+                System.out.print("명언 : ");
+                String 명언 = s.nextLine();
+                System.out.print("작가 : ");
+                String 작가 = s.nextLine();
+            }
+        }
+
     }
 }
